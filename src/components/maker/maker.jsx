59cb3059e24810ab3e40c6b5a04984450cfd62a1
CyCle <cycle5328@naver.com>
2022-02-07 13:19:15 +0900

    add card and edit form component

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Editor from "../editor/editor";
 import Footer from "../footer/footer";
@@ -7,6 +7,41 @@ import Preview from "../preview/preview";
 import styles from "./maker.module.css";
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: "1",
+      name: "cycle",
+      company: "none",
+      theme: "dark",
+      title: "Frontend Engineer",
+      email: "cycle5328@Gmail.com",
+      message: "fall forward",
+      fileNmae: "CyCle",
+      fileURL: null,
+    },
+    {
+      id: "2",
+      name: "cycle2",
+      company: "none",
+      theme: "light",
+      title: "Frontend Engineer",
+      email: "cycle5328@Gmail.com",
+      message: "fall forward",
+      fileNmae: "CyCle",
+      fileURL: "cycle.png",
+    },
+    {
+      id: "3",
+      name: "cycle3",
+      company: "none",
+      theme: "colorful",
+      title: "Frontend Engineer",
+      email: "cycle5328@Gmail.com",
+      message: "fall forward",
+      fileNmae: "CyCle",
+      fileURL: null,
+    },
+  ]);
   const navigate = useNavigate();
   const onLogout = () => {
     authService.logout();
@@ -24,8 +59,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
